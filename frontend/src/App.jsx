31fc876b8957feaff02001a4f3c70dbb8755d00d
hokageCodes <hashsenju94@gmail.com>
2023-05-07 15:05:03 +0100

    Added logic for when user is signed in, signedup or not and where they should be redirected to in those instances

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,35 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { useState } from 'react';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  const login = (userData) => {
+    setUser(userData);
+  };
+
+  const logout = () => {
+    setUser(null);
+  };
+
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<LoginPage />} />
-        <Route path='/signup' element={<SignupPage />} />
+        <Route
+          path='/login'
+          element={user ? <Navigate to="/" /> : <Login login={login} />}
+        />
+        <Route
+          path='/signup'
+          element={user ? <Navigate to="/" /> : <Signup login={login} />}
+        />
+        <Route
+          path='/'
+          element={user ? <TodoList user={user} logout={logout} /> : <Navigate to="/login" />}
+        />
       </Routes>
     </Router>
   );
